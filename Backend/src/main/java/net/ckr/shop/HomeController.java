244d45c68796477ec47b5d27c53cd6563fbe0c7e
Mikhail Vilms <mihail.vilms@gmail.com>
2017-02-16 17:05:12 +0400

    Signed-off-by: Mikhail Vilms <mihail.vilms@gmail.com>

---
+++ Backend/src/main/java/net/ckr/shop/HomeController.java
@@ -0,0 +1,23 @@
+package net.ckr.shop;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String home() {
+	
+		String hw="Hello World!";	
+		
+		return hw;
+	}
+	
+}
