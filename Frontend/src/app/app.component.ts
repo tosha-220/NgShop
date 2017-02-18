8d674cdfe3c7c43bdd70da4a1dae2eaab8b76450
tosha220 <tosha-220@yandex.ru>
2017-02-18 14:08:02 +0300

    service with json added

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component,OnInit} from '@angular/core';
+import {HelloService} from './app.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  providers: [HelloService]
+})
+export class AppComponent implements OnInit{ 
+  value=["offline"];
+  constructor(private helloService: HelloService) { 
+    }
+
+  ngOnInit(): void {
+    this.helloService.toDo().then(value=>this.value=value);
+  }
+}
