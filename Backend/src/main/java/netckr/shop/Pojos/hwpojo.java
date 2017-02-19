d87eca5dbdfa18cf5ddb08e8e6b23226e1788340
Mikhail Vilms <mihail.vilms@gmail.com>
2017-02-19 19:14:10 +0400

    Signed-off-by: Mikhail Vilms <mihail.vilms@gmail.com>

---
+++ Backend/src/main/java/netckr/shop/Pojos/hwpojo.java
@@ -0,0 +1,24 @@
+package netckr.shop.Pojos;
+ 
+public class hwpojo {
+ 
+    private int id;
+    private String str;
+    
+    public hwpojo() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	 
+}
