4f880b32f8c9b627ccc4d814bcce5acc608295c5
tosha220 <tosha-220@yandex.ru>
2017-03-07 21:59:38 +0300

    link in file, ngprime-list, handleError added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from "@angular/core";
+import {NgModule, APP_INITIALIZER} from "@angular/core";
 import {HttpModule} from "@angular/http";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
@@ -6,12 +6,20 @@ import {ProductComponent} from "./product/product.component";
 import {ProductsComponent} from "./products/products.component";
 import {AppRoutingModule} from "./app-routing.module";
 import {ProductService} from "./product.service";
+import {CarouselModule,ButtonModule,DialogModule,GrowlModule} from 'primeng/primeng';
+import {RouteService} from "./route.service";
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule],
+  imports: [BrowserModule, HttpModule, AppRoutingModule,CarouselModule,ButtonModule,DialogModule,GrowlModule],
   declarations: [AppComponent, ProductComponent, ProductsComponent],
   bootstrap: [AppComponent],
-  providers: [ProductService]
-})
+  providers: [ProductService,RouteService,
+  {
+    provide: APP_INITIALIZER,
+    useFactory: (route: RouteService) => { return () => route.init() },
+    deps: [RouteService],
+    multi: true
+  }
+]})
 export class AppModule {
 }
--- Frontend/src/app/products/products.component.ts
+++ Frontend/src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import {Product} from "../api/product";
 import {ProductService} from "../product.service";
+import {Message} from "primeng/components/common/api";
 
 @Component({
   moduleId: module.id,
@@ -12,13 +13,15 @@ import {ProductService} from "../product.service";
 export class ProductsComponent implements OnInit {
   products: Product[];
   selectedProduct: Product;
+  displayDialog: boolean;
+  msgs: Message[] = [];
 
   constructor(private router: Router,
               private productService: ProductService) {
   }
 
   getAll(): void {
-    this.productService.getAll().subscribe(r => this.products = r);
+    this.productService.getAll().subscribe(r => this.products = r,error =>  this.msgs = error);
   }
 
   ngOnInit(): void {
@@ -27,9 +30,16 @@ export class ProductsComponent implements OnInit {
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
+    this.showDialog();
   }
 
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedProduct.id]);
+  }
+  onDialogHide() {
+    this.selectedProduct = null;
+  }
+  showDialog() {
+    this.displayDialog = true;
   }
 }
